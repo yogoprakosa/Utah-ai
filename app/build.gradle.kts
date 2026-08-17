89ff3684245ddcabd5059b6f3dcb3d329cf7a076
yogoprakosa <yogoprakosa@gmail.com>
2026-08-17 22:19:27 +0700

    Fix Material3 dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.12.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
